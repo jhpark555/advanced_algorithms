84ba0067f6a302c05314d2496bd0cc5210af89e2
Jaehong Park <jhpark555@gmail.com>
2021-07-05 20:40:25 -0700

    update

---
+++ frequency_of_all_element_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void findFrequency(int arr[], int n)
+{
+  int map[11]={0};
+  int i;
+
+  for(i=0;i<n;i++)
+  {
+    map[arr[i]]++;
+  }
+
+  for(i=0;i<n;i++)
+  {
+    if(map[i])
+     printf("arr[%d]=%d \n",i,map[i]);
+  }
+
+
+}
+int main()
+{
+//  int arr[]={2,3,3,2,1};
+ int arr[]={1, 1, 1, 2, 3, 3, 5, 5, 8, 8, 8, 9, 9, 10};
+  int n=sizeof(arr)/sizeof(arr[0]);
+
+  findFrequency(arr, n);
+}
