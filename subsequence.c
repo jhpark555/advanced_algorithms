4261d26c5fc38ea4e237a390ccbe93331c4c27fb
Jaehong Park <jhpark555@gmail.com>
2021-07-13 23:18:11 -0700

    subsequence

---
+++ subsequence.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void findPowerSet( char s[],int n)
+{
+  int i,j;
+  int m=pow(2,n);
+
+  for(i=0;i<m;i++)
+  {
+    printf("(");
+    for(j=0;j<m;j++)
+    {
+      if( i & (1<<j) )    // why?
+      printf("%c",s[j]);
+    }
+    printf(")");
+  }
+
+}
+
+int main()
+{
+  char str[]="apple";
+  int n=strlen(str);
+  findPowerSet(str,n);
+
+  return 0;
+}
